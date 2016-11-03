86959c77c8c741f4fcf8a141084eebfdb7ace657
thebridge0491 <thebridge0491-codelab@yahoo.com>
2016-11-02 23:21:48 -0400

    User interface (desktop environment) provisioning VM (CentOS, PCLinuxOS, openSUSE, Mageia)

---
+++ scripts/opensuse/user_ifc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+set +e
+
+CHOICE_DESKTOP=${CHOICE_DESKTOP:-lxde}
+
+zypper --non-interactive refresh ; zypper --non-interactive update
+. /root/distro_pkgs.txt
+case $CHOICE_DESKTOP in
+	*) pkgs_var=$pkgs_deskenv_lxde ;;
+esac
+
+zypper --non-interactive install --download-only --no-recommends $pkgs_var
+for pkgX in $pkgs_var ; do
+	zypper --non-interactive install --no-recommends $pkgX ;
+done
+sleep 3
+
+case $CHOICE_DESKTOP in
+	*) mv /etc/lightdm /etc/lightdm.old ;
+	  systemctl enable lightdm ;;
+esac
+systemctl set-default graphical.target ; sleep 3
+
+# update XDG user dir config
+export LANG=en_US.UTF-8 ; export CHARSET=UTF-8
+echo 'BIN=bin' >> /etc/xdg/user-dirs.defaults
+xdg-user-dirs-update ; sleep 3
---
+++ scripts/pclinuxos/user_ifc.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+set +e
+
+CHOICE_DESKTOP=${CHOICE_DESKTOP:-lxde}
+
+apt-get update ; apt-get -y dist-upgrade
+. /root/distro_pkgs.txt
+case $CHOICE_DESKTOP in
+	*) pkgs_var=$pkgs_deskenv_lxde ; chkconfig --add slim ;;
+esac
+
+apt-get -y --option Retries=3 install drakconf acpi acpid $pkgs_var
+# fix AND re-attempt install for infrequent errors
+apt-get -y --fix-broken install
+apt-get -y --option Retries=3 install drakconf acpi acpid $pkgs_var
+sleep 3
+
+#XFdrake --auto
+drakx11 ; sleep 5 ; drakdm ; sleep 5 ; drakboot ; sleep 500
+mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bak
+
+# update XDG user dir config
+export LANG=en_US.UTF-8 ; export CHARSET=UTF-8
+echo 'BIN=bin' >> /etc/xdg/user-dirs.defaults
+xdg-user-dirs-update ; sleep 3
